07177bc05a72fc96b2c05e3b93c6effc36143b64
raouf iatarien <raoufkouaider@gmail.com>
2024-04-02 14:02:01 +0100

    f

--- resources/views/rapport/ajouter_outils.blade.php
+++ resources/views/rapport/ajouter_outils.blade.php
@@ -87,7 +87,7 @@
                                     <div class="form-group" align="center">
                                     <button class="btn btn-primary" type="button" onclick="add_device()">تثبيت الجهاز</button>
                                     </div>
-                                </from>
+                                </form>
                                 
                             </div>
                         
@@ -170,7 +170,7 @@
                                         <div class="form-group" align="center">
                                         <button class="btn btn-primary" type="button" onclick="add_matiere()">  تثبيت الأداة</button>
                                         </div>
-                                    </from>
+                                    </form>
                                     
                                 </div>
                             
@@ -195,13 +195,25 @@
                                 </div>
                             </div>
                             <hr>
+
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">  المواد الكيميائية</h6>
                                 </div>
-                                <div class="row">
+                                <br>
+                                <div class="form-group row">
+                                    <label class="control-label col-lg-2 text-right" for="title">  قائمة المواد الكيميائية  </label>
+                                    <div class="col-lg-6">
+                                    <select class="form-control" name="show_chemicals" id="show_chemicals" onchange="show_chemicals(this.value)">
+                                        <option> لا شيئ</option>  
+                                        <option value="show">اسم المواد</option>
+                                    </select>
+                                    </div>
+                                </div>
+                                
+                                <div class="row" id="chemicals_view" style="display : none">
                                     <div class="card-body col-md-6">
-                                        <form id="chemicals_form" class="form-horizontal" onsubmit="return false;"> >
+                                        <form id="chemicals_form" onsubmit="return add_chemical(event);">
                                             
                                             <div class="form-group row">
                                                 <label class="control-label col-lg-4 text-right" for="title"> المادة </label>
@@ -216,13 +228,13 @@
                                             <div class="form-group row">
                                                 <label class="control-label col-lg-4 text-right" for="title">  الوحدة</label>
                                                 <div class="col-lg-8">
-                                                    <input type="text" value="0" required id="unity" class="form-control">
+                                                    <input type="text" value="" required id="unity" class="form-control">
                                                 </div>
                                             </div>
                                             <div class="form-group row">
                                                 <label class="control-label col-lg-4 text-right" for="title">   الكمية </label>
                                                 <div class="col-lg-8">
-                                                    <input type="number" value="0" required id="quantity" class="form-control">
+                                                    <input type="number" value="" required id="quantity" class="form-control">
                                                 </div>
                                             </div>
 
@@ -251,7 +263,9 @@
                                             </table>
                                         </div>
                                     </div>
-                        </div>
+                                    
+                            </div>
+                            <br><br>
                     </div>
 
                 </div>
@@ -265,13 +279,13 @@
 function add_matiere(){
     matiere = document.getElementById('matiere');
     matieres = matiere.value.split('1989raouf1989')[1];
+    id = matiere.value.split('1989raouf1989')[0];
     charge = document.getElementById('charge_m');
     state_avant = document.getElementById('state_avant_m');
     avis = document.getElementById('avis_m');
     state_after = document.getElementById('state_after_m');
     str = document.getElementById('t_matiere_devices').innerHTML;
     str +="<tr>"+
-    "<tr>"+
         "<td>"+matieres+"</td>"+
         "<th>"+charge.value+"</th>"+
         "<th>"+state_avant.value+"</th>"+
@@ -284,13 +298,13 @@ function add_matiere(){
 function add_device(){
     device = document.getElementById('device');
     devices = device.value.split('1989raouf1989')[1];
+    id = device.value.split('1989raouf1989')[0];
     charge = document.getElementById('charge');
     state_avant = document.getElementById('state_avant');
     avis = document.getElementById('avis');
     state_after = document.getElementById('state_after');
     str = document.getElementById('t_body_devices').innerHTML;
     str +="<tr>"+
-    "<tr>"+
         "<td>"+devices+"</td>"+
         "<th>"+charge.value+"</th>"+
         "<th>"+state_avant.value+"</th>"+
@@ -304,22 +318,20 @@ function add_device(){
 function add_chemical(e){
     e.preventDefault();
     console.log(e);
-    
-    // alert('');
-    // chemical = document.getElementById('chemical');
-    // chemicals = chemical.value.split('1989raouf1989')[1];
-    // unity = document.getElementById('unity');
-    // quantity = document.getElementById('quantity');
-    // str = document.getElementById('t_body_chemicals').innerHTML;
-    // str +="<tr>"+
-    // "<tr>"+
-    //     "<td>"+chemicals+"</td>"+
-    //     "<th>"+unity.value+"</th>"+
-    //     "<th>"+quantity.value+"</th>"+
-    // "</tr>";
-    // document.getElementById('t_body_chemicals').innerHTML = str;
-    // document.getElementById('chemicals_form').reset();
-    // return false;
+    chemical = document.getElementById('chemical');
+    chemicals = chemical.value.split('1989raouf1989')[1];
+    id = chemical.value.split('1989raouf1989')[0];
+    unity = document.getElementById('unity');
+    quantity = document.getElementById('quantity');
+    str = document.getElementById('t_body_chemicals').innerHTML;
+    str +="<tr>"+
+        "<td>"+chemicals+"</td>"+
+        "<th>"+unity.value+"</th>"+
+        "<th>"+quantity.value+"</th>"+
+    "</tr>";
+    document.getElementById('t_body_chemicals').innerHTML = str;
+    document.getElementById('chemicals_form').reset();
+    return false;
 
 }
 function changed_activity(val){
@@ -327,6 +339,14 @@ function changed_activity(val){
         document.getElementById('some').style.display ='none';
     }else{
         document.getElementById('some').style.display ='flex';
+    } 
+}
+
+function show_chemicals(val){
+    if(val =="show"){
+        document.getElementById('chemicals_view').style.display ='flex';
+    }else{
+        document.getElementById('chemicals_view').style.display ='none';
     } 
 }
 window.onload = function(){
